122448944def227cd360975da9a3fc63dfdcf631
Aria Buckles <aria@seesaw.me>
2019-09-09 17:25:41 -1000

    Don't remove variables that have non-pure inits

--- src/visitors/unused-variables.js
+++ src/visitors/unused-variables.js
@@ -4,18 +4,24 @@ module.exports = {
   VariableDeclarator: {
     enter(path, state) {
       const id = path.node.id;
-      const binding = path.scope.getOwnBinding(id.name);
       const parent = path.parent;
+      const binding = path.scope.getOwnBinding(id.name);
+
       if (binding == null) {
         return;
       }
-      if (!binding.referenced) {
-        path.scope.removeOwnBinding(id.name);
-        if (t.isVariableDeclaration(parent.node) && parent.node.declarations.length <= 1) {
-          parent.remove();
-        } else {
-          path.remove();
-        }
+      if (!path.get('init').isPure()) {
+        return;
+      }
+      if (binding.referenced) {
+        return;
+      }
+
+      path.scope.removeOwnBinding(id.name);
+      if (t.isVariableDeclaration(parent.node) && parent.node.declarations.length <= 1) {
+        parent.remove();
+      } else {
+        path.remove();
       }
     }
   }
