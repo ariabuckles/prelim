c7b54c1bd8ed64b01180526ceba48f3fb1b67574
Aria Buckles <aria@toole1.com>
2019-11-23 16:52:27 -0800

    Tests: Add snapshot testing

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: 'babel',
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
---
+++ src/tests/fixtures/unused-properties.js
@@ -0,0 +1,2 @@
+let object1 = { a: console.log(1), b: 2, 'c': 3, d: 4 };
+console.log(object1.b);
